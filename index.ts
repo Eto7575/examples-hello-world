0e6ccb2d9421687cfa31d7b447b98c19e9b4b5b5
Eto7575 <kaya.ertugrul.hamza@gmail.com>
2025-10-09 01:15:39 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,38 +1,41 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { connect } from "https://deno.land/x/redis@v0.31.0/mod.ts";
 
-const redis = await connect({
-  hostname: "frankfurt-keyvalue.render.com",
-  port: 6379,
-  password: "AfL6jiHOzZnSCGKCrvRSxCU9Tt6xq7d5",
-  tls: true,
-});
+async function main() {
+  const redis = await connect({
+    hostname: "frankfurt-keyvalue.render.com",
+    port: 6379,
+    password: "AfL6jiHOzZnSCGKCrvRSxCU9Tt6xq7d5",
+    tls: true,
+  });
 
-const clients = new Set<WebSocket>();
+  const clients = new Set<WebSocket>();
 
-serve((req) => {
-  const { socket, response } = Deno.upgradeWebSocket(req);
+  serve((req) => {
+    const { socket, response } = Deno.upgradeWebSocket(req);
 
-  socket.onopen = () => {
-    clients.add(socket);
-    console.log("Yeni bağlantı!");
-  };
+    socket.onopen = () => {
+      clients.add(socket);
+      console.log("Yeni bağlantı!");
+    };
 
-  socket.onmessage = async (e) => {
-    // Mesajı Redis'e kaydet
-    await redis.publish("cloudlink", e.data);
-  };
+    socket.onmessage = async (e) => {
+      await redis.publish("cloudlink", e.data);
+    };
 
-  socket.onclose = () => clients.delete(socket);
-  return response;
-});
+    socket.onclose = () => clients.delete(socket);
+    return response;
+  });
 
-// Redis kanalından gelen mesajları herkese gönder
-(async () => {
-  const sub = await redis.subscribe("cloudlink");
-  for await (const { message } of sub.receive()) {
-    for (const client of clients) {
-      client.send(message);
+  // Redis'ten mesaj dinle
+  (async () => {
+    const sub = await redis.subscribe("cloudlink");
+    for await (const { message } of sub.receive()) {
+      for (const client of clients) {
+        client.send(message);
+      }
     }
-  }
-})();
+  })();
+}
+
+main();
